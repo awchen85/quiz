b90125cdd4c6ee785752466355993d121e2f25ae
awchen <awchen85@gmail.com>
2022-08-21 19:38:16 -0400

    added timer functionality

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,13 +1,46 @@
-generateButton.addEventListener("click", timer)
 
+//variable to store time
+var remainingTime = 75;
 
-function timer() {
-    var sec = 75;
-    var timer = setInterval(function() {
-        document.getElementById('timerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
+//variable to track whether timer is runnong or not
+var isStopped = true;
+
+//Action Start
+const startButton = document.getElementById("start-quiz");
+
+const countContainer = document.getElementById("timerDisplay");
+
+//start timer
+const startTimer = () => {
+    if (isStopped) {
+        isStopped = false;
+        countContainer.innerHTML = remainingTime;
+        timer = setInterval(renderTime, 1000);
+    }
 }
+//stop timer
+const stopTimer = () => {
+    isStopped = true;
+    if (timer) {
+        clearInterval(timer);
+    }
+};
+
+startButton.onclick = startTimer;
+
+//function to display time
+const renderTime = () => {
+    remainingTime -= 1;
+//render timer on screen
+countContainer.innerHTML = remainingTime;
+//timeout on zero
+if (remainingTime === 0) {
+    isStopped = true;
+    clearInterval(timer);
+}
+};
+
+
+
+
+
